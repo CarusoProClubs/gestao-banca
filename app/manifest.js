e42356c8307f2b1e5a279a832e876021bae13443
CarusoProClubs <316124569+CarusoProClubs@users.noreply.github.com>
2026-09-17 22:15:18 -0300

    pwa: use committed application icons

--- app/manifest.js
+++ app/manifest.js
@@ -10,9 +10,9 @@ export default function manifest() {
     theme_color: "#071018",
     orientation: "portrait-primary",
     icons: [
-      { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
-      { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
-      { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png", purpose: "maskable" }
+      { src: "/icons/icon-192.svg", sizes: "192x192", type: "image/svg+xml" },
+      { src: "/icons/icon-512.svg", sizes: "512x512", type: "image/svg+xml" },
+      { src: "/icons/icon-512.svg", sizes: "512x512", type: "image/svg+xml", purpose: "maskable" }
     ]
   };
 }
